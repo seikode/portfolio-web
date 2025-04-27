e8424ef809eef3ee32e8ba602e69c170309c4a04
seikode <64799923+seikode@users.noreply.github.com>
2025-04-27 15:58:24 +0900

    🎨 update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,25 +5,33 @@ const Header = () => {
   const [navOpen, setNavOpen] = useState(false);
   return (
     <header
-      className="fixed top-0 left-0 w-full h-20 flex
+      className="fixed top-10 left-0 w-full h-20 flex
   items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0"
     >
       <div
         className="max-w-screen-2xl w-full mx-auto px-4
-      flex justify-between items-center md:px-6 md:grid-cols-[1fr,3fr,1fr]"
+      flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr_3fr_1fr]"
       >
         <h1>
           <a href="/" className="logo">
             <img src="/images/logo.svg" width={40} height={40} alt="Seikode" />
           </a>
         </h1>
-        <div className="">
-          <button className="menu-btn md:hidden" onClick={null}>
-            <span className="material-symbols-rounded">menu</span>
+        <div className="relative md:justify-self-center">
+          <button
+            className="menu-btn md:hidden"
+            onClick={() => setNavOpen((prev) => !prev)}
+          >
+            <span className="material-symbols-rounded">
+              {navOpen ? "close" : "menu"}
+            </span>
           </button>
           <Navbar navOpen={navOpen} />
         </div>
-        <a href="#contact" className="btn btn-secondary max-md:hidden">
+        <a
+          href="#contact"
+          className="btn btn-secondary max-md:hidden md:justify-self-end"
+        >
           Contact Me
         </a>
       </div>
